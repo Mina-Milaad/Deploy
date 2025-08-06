a6fc2e5dad281eabfad84dc2da4ead6e10ac8b9b
Mina-Milaad <minaamilaad4@gmail.com>
2025-08-06 13:17:31 +0300

    deploy server

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 
 
 
-export const dbConn = connect('mongodb://host.docker.internal:27017/deploy-Project')
+export const dbConn = connect('mongodb://mongo:27017/deploy-Project')
 .then(() => {
     console.log("database Connected Successfully");
 })
